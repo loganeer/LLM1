d0bea783557cf57b9bf909be3fe137b1a37bed1c
Nik Trapani <loganeer@gmail.com>
2024-12-20 18:54:18 +0900

    added unknown and new text tokens

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
-all_words = sorted(set(preprocessed))
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer, token in enumerate(all_tokens)}
 
-vocab = {token:integer for integer, token in enumerate(all_words)}
 for i,item in enumerate(vocab.items()):
     print(item)
     if i>=50:
         break
 
 tokenizer = SimpleTokenizerV1(vocab)
+text = "Hello, do you like tea?"
+ids = tokenizer.encode(text)
+print(ids)
+print(tokenizer.decode(ids))
--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,4 @@
+import re
 class SimpleTokenizerV1:
     def __init__(self, vocab):
         self.str_to_int = vocab
@@ -8,13 +9,15 @@ class SimpleTokenizerV1:
         preprocessed = [
             item.strip() for item in preprocessed if item.strip()
         ]
+
+        preprocessed = [item if item in self.str_to_int
+                        else "<|unk|>" for item in preprocessed]
+
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
     
     def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
 
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
-    
-
